4252693b919e71cf926034a595da0cdcb964c0d4
Elias Wickander <elias.wickander@gmail.com>
2023-03-03 21:29:54 +0100

    Added SceneAttribute

--- Assets/Scripts/Util/SceneLoader.cs
+++ Assets/Scripts/Util/SceneLoader.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Util.Attributes;
 
 public class SceneLoader : MonoBehaviour
 {
+    [Scene]
     public string m_sceneToLoad;
     
     public void Load()
