8f41bb7cabd08387f50718a66029a1a34586cb56
Elias Wickander <elias.wickander@gmail.com>
2023-03-03 22:05:40 +0100

    Added Singleton util

--- Assets/Scripts/Util/AdvancedTypes/Singleton.cs
+++ Assets/Scripts/Util/AdvancedTypes/Singleton.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+namespace Util.AdvancedTypes
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
+        public bool m_persistentBetweenScenes;
+        private static readonly object m_lock = new object();
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private static T s_instance;
+        public static T Instance
+        {
+            get
+            {
+                lock (m_lock)
+                {
+                    if (s_instance != null)
+                        return s_instance;
+    
+                    T[] instances = FindObjectsOfType<T>();
+    
+                    if (instances.Length > 0)
+                    {
+                        if (instances.Length > 1)
+                        {
+                            Debug.LogError($"Found {instances.Length} duplicates of Singleton<{typeof(T)}>. Cleaning all but one");
+    
+                            for (int i = 1; i < instances.Length; i++)
+                            {
+                                Destroy(instances[i]);
+                            }
+                        }
+    
+                        s_instance = instances[0];
+                        return s_instance;
+                    }
+                    else
+                    {
+                        Debug.LogError($"Failed to find instance of Singleton<{typeof(T)}> in scene. Creating one...");
+                        
+                        GameObject newObject = new GameObject($"Singleton<{typeof(T)}>");
+    
+                        s_instance = newObject.AddComponent<T>();
+    
+                        return s_instance;
+                    }
+                }
+            }
+        }
+    
+        private void Awake()
+        {
+            if(m_persistentBetweenScenes)
+                DontDestroyOnLoad(gameObject);
+            
+            OnAwake();
+        }
+    
+        protected virtual void OnAwake()
+        {
+            
+        }
     }
 }
