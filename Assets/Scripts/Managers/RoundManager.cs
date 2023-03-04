6c9a8ba940f7853c7351c6665c07d67fa738c3df
Elias Wickander <elias.wickander@gmail.com>
2023-03-04 17:15:06 +0100

    Set up player scripts and round manager

--- Assets/Scripts/Managers/RoundManager.cs
+++ Assets/Scripts/Managers/RoundManager.cs
@@ -1,8 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Util.AdvancedTypes;
 
-public class RoundManager : Singleton
+[Serializable]
+public class RoundData
 {
+    public string m_roundName;
+}
 
+public class RoundManager : Singleton<RoundManager>
+{
+    public event Action<RoundData> OnRoundStartEvent;
+
+    public List<RoundData> m_rounds = new List<RoundData>();
+
+    private int m_activeRoundIndex = -1;
+
+    private void Start()
+    {
+        StartNextRound();
+    }
+
+    private void StartNextRound()
+    {
+        if (m_rounds.Count <= m_activeRoundIndex + 1)
+        {
+            Debug.LogError("Cannot start next round. No more rounds");
+            return;
+        }
+        
+        m_activeRoundIndex++;
+        OnRoundStartEvent?.Invoke(m_rounds[m_activeRoundIndex]);
+        
+        Debug.Log($"Starting round {m_rounds[m_activeRoundIndex].m_roundName}");
+    }
 }
--- Assets/Scripts/Player/PlayerSpawner.cs
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] 
+    private PlayerData m_playerData;
+
+    private void OnEnable()
     {
-        
+        RoundManager.Instance.OnRoundStartEvent += OnRoundStart;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        RoundManager.Instance.OnRoundStartEvent -= OnRoundStart;
+    }
+    
+    private void Spawn()
+    {
+        if (m_playerData == null)
+        {
+            Debug.LogError("Cannot spawn player. Player data is null", gameObject);
+            return;
+        }
         
+        if (m_playerData.m_prefab == null)
+        {
+            Debug.LogError("Cannot spawn player. Prefab in player data is null", gameObject);
+            return;
+        }
+
+        Instantiate(m_playerData.m_prefab, transform.position, Quaternion.identity);
+    }
+    
+    private void OnRoundStart(RoundData round)
+    {
+        Spawn();
     }
 }
